6170e591dbdd88df449945844c88c9c64138288f
MrMirror21 <mylittlesentry@gmail.com>
2020-03-10 03:43:02 +0900

    Update 1-3.Network_packet_processing.cpp
    1-3 : solved

--- ProgrammingChallenge1/1-3.Network_packet_processing.cpp
+++ ProgrammingChallenge1/1-3.Network_packet_processing.cpp
@@ -3,6 +3,34 @@
 using namespace std;
 
 int main() {
+	int Size_buffer = 0;
+	int numOfPacket = 0;
+	cin >> Size_buffer >> numOfPacket;
+	int** buffer = new int* [numOfPacket];
+	for (int i = 0; i < numOfPacket; i++)
+		buffer[i] = new int[2];
+
+	for (int i = 0; i < numOfPacket; i++) {
+		int _ArriveTime;
+		int _ProcessingTime;
+		cin >> _ArriveTime >> _ProcessingTime;
+		buffer[i][0] = _ArriveTime;
+		buffer[i][1] = _ProcessingTime;
+	}
+
+	int TimeNow = 0;
+	for (int i = 0; i < numOfPacket; i++) {
+		if (TimeNow > buffer[i][0]) {
+			cout << -1 << endl;
+			continue;
+		}
+		while (TimeNow != buffer[i][0]) {
+			TimeNow++;
+		}
+		cout << TimeNow << endl;
+		TimeNow += buffer[i][1];
+	}
+	
 
 	return 0;
 }
